01f5b16210e8726d10b95472a4dce7173c47318c
Jonius7 <jhgenius01@hotmail.com>
2024-03-14 11:57:08 +1100

    Add CSS classes to change number of cards

--- src/CardHandBoard.js
+++ src/CardHandBoard.js
@@ -12,17 +12,26 @@ const CardHandBoard = (props) => {
     const [cardPosition, setCardPosition] = useState(props.cardPosition);
     const [cardPosition2, setCardPosition2] = useState(props.cardPosition);
     
-    const [selectedOption, setSelectedOption] = useState();
+    const [selectedOption1, setSelectedOption] = useState();
     const [selectedOption2, setSelectedOption2] = useState();
     const [selectedOption3, setSelectedOption3] = useState();
     const [selectedOption4, setSelectedOption4] = useState();
     const [selectedOption5, setSelectedOption5] = useState();
-    const [selectedOptions, setSelectedOptionsList] = useState([selectedOption, selectedOption2]);
+    const [selectedOptions, setSelectedOptionsList] = useState([
+        { id: 1, value: 'card1' }, { id: 2, value: 'card2' }, 
+        { id: 3, value: 'card3' }, { id: 4, value: 'card4' }, { id: 5, value: 'card5' }
+    ]);
     const [player, setPlayer] =  useState(1);
     const [player2, setPlayer2] =  useState(2);
     const [options, setOptions] =  useState([]);
     const [options2, setOptions2] =  useState([]);
     const [NumberofCards, setNumberofCards] = useState(5);
+    const cardElements = [];
+    for (let i = 0; i < NumberofCards; i++) {
+        const selectedOptionName = "selectedOption" + NumberofCards;
+        //cardElements.push(<Card selectedOption={selectedOption1}/>);
+        cardElements.push(<Card selectedOption={selectedOptions[NumberofCards]}/>);
+    }
     console.log(variables);
     //how to separate to create multiple instances
 
@@ -71,15 +80,23 @@ const CardHandBoard = (props) => {
       setSelectedOptionsList(e.value);
     }
 
+    const handleSelectedOptionsListChange = (id) => {
+        setSelectedOptionsList((prevItems) =>
+            prevItems.map((item) =>
+                item.id === id ? { ...item} : item
+            )
+        );
+    };
+
     const handleIncNumberofCards = () => {
       if (NumberofCards < 10) {
-          setNumberofCards((parseInt(variables.numberofcards) + 1).toString())
+          setNumberofCards(NumberofCards + 1)
       }
   }
 
   const handleDecNumberofCards = () => {
       if (NumberofCards > 0) {
-          setNumberofCards((parseInt(variables.numberofcards) - 1).toString())
+        setNumberofCards(NumberofCards - 1)
       }
   }
 
@@ -93,12 +110,12 @@ const CardHandBoard = (props) => {
                           <p>Number of Cards</p>
                           <button onClick={handleIncNumberofCards}>+</button>
                           <button onClick={handleDecNumberofCards}>-</button>
-                          <p>{variables.numberofcards}</p>
+                          <p>{NumberofCards}</p>
                       </td>
                       <td class="narrowTd">
                           <Select
                               className="cards"
-                              defaultValue={selectedOption}
+                              defaultValue={selectedOption1}
                               onChange={handleSetSelectedOption}
                               options={options}
                               components={{
@@ -150,13 +167,8 @@ const CardHandBoard = (props) => {
                 </table>
             </div>
 
-            <div className="cardshand">
-                <Card selectedOption={selectedOption}/>
-                <Card selectedOption={selectedOption2}/>
-                <Card selectedOption={selectedOption}/>
-                <Card selectedOption={selectedOption2}/>
-                <Card selectedOption={selectedOption3}/>
-                <Card />
+            <div className={`cardshand cards-${NumberofCards}`}>
+                {cardElements}
             </div>
         </div>
     );
